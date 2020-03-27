37eb68a7a7dd9606a97ffc71ee53a1fbee16c4af
samirsherlekar <sas450@pitt.edu>
2020-03-26 20:34:32 -0400

    deprecated demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React , { Component } from 'react';
 import './App.css';
-import Demo from "./components/Demo"
 import Recipe from "./components/Recipe"
 import Search from "./components/Search"
+
 class App extends Component {
   state = {
     user_ingredients : []

3a9c0ccb254ae4f814844cc1e6e92c970441869b
samirsherlekar <sas450@pitt.edu>
2020-04-04 18:54:06 -0400

    ok

---
+++ server.js
@@ -0,0 +1,8 @@
+//imports functionality old way instead of from .... import ...
+const http = require("http");
+const app = require("./app");
+
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
+
+server.listen(port);

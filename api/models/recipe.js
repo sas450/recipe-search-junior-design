e1843b70d9b580654f784dea06a05217571cb7a7
samirsherlekar <sas450@pitt.edu>
2020-04-04 20:00:32 -0400

    fixed bb json file location

--- api/models/recipe.js
+++ api/models/recipe.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-var RecipeBag = require("/Users/samirsherlekar/Documents/Projects/recipe_search_engine/data/bb.json");
+var RecipeBag = require("./bb.json");
 
 const recipeSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
